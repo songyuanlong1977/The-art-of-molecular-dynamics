eb4c2a071a7da8e2466340bc427085b1342115d9
songyuanlong <songyuanlong@hotmail.com>
2025-01-12 22:09:58 +0800

    body centered cubic coordinate initialization

--- InitCoords.c
+++ InitCoords.c
@@ -1,27 +1,34 @@
 #include "head.h"
 void InitCoords(void)
 {
+/*
+	body-centered cubic (BCC) lattice has two atoms per unit cell
+*/
 	VecR c,gap;
 	/*gap: */
-	int n, nx,ny,nz;
+	int j,n, nx,ny,nz;
 	VDiv(gap,region,initUcell);
 	/*region hold the size of a square */
 
 	n=0;
-	for (ny=0;ny<initUcell.y; ny++)
+	for (nz=0;nz<initUcell.z; nz++)
 	{
-		for (nx=0;nx<initUcell.x;nx++)
+		for (ny=0;ny<initUcell.y;ny++)
 		{
-			for(nz=0;nz<initUcell.z;nz++)
+			for(nx=0;nx<initUcell.x;nx++)
 			{
-				VSet(c,nx+0.5,ny+0.5,nz+0.5);
+				VSet(c,nx+0.25,ny+0.25,nz+0.25);
 				/*c is the normalied coordinate of the unit cells*/
 				VMul(c,c,gap);
 				/*c is coordinate mapped to the region*/
 				VVSAdd(c,-0.5,region);
 				/* make origin the center of region */
-				mol[n].r=c;
-				++n;
+				for(j=0;j<2;j++)
+				{
+					mol[n].r=c;
+					if(j==1) VVSAdd(mol[n].r,0.5,gap);
+					++n;
+				}
 			}
 		}
 	}
